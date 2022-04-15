f54d231ba99f4fa86d2e3ea6f338db576a625373
hx-natthawat <natthawat@harmonyx.co>
2022-04-15 11:56:29 +0700

    Add new schem for sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { cloneElement } from 'react'
+import { sanityClient, urlFor } from '../sanity'
+import { Collection } from '../sanity/typings'
 
-const Home: NextPage = () => {
+interface Props {
+  collections: Collection
+}
+
+const Home = ({ collections }: Props) => {
   return (
     <div className="">
       <Head>
@@ -10,9 +17,47 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className='text-red-500'>Welcome to the NFT DROP CHALLENGE</h1>
+      <h1 className="text-red-500">Welcome to the NFT DROP CHALLENGE</h1>
     </div>
   )
 }
 
 export default Home
+
+// Call Backend server to render as a SSR mode
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const query = `
+  *[_type == "collection"]{
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage {
+      asset
+    },
+    previewImage {
+      asset
+    },
+    slug {
+      current
+    },
+    creator-> {
+      _id,
+      name,
+      address,
+      slug {
+        current
+      },
+    },
+  }`
+
+  const collections = await sanityClient.fetch(query)
+  console.log(collections)
+
+  return {
+    props: {
+      collections,
+    },
+  }
+}
